6021a540a2962991eecf641036efa2e5dc110135
hugo_xu <120097527@qq.com>
2016-11-23 10:40:06 +0800

    完成朴素贝叶斯分类器

--- naive-Bayes-classifiers.py
+++ naive-Bayes-classifiers.py
@@ -28,26 +28,39 @@ for i in range(n):
         badWM += 1
 
 P_goodWM = (goodWM + 1)/(n + 2)
-p_badWM = (badWM + 1)/(n + 2)
+P_badWM = (badWM + 1)/(n + 2)
+
+#取得拉普拉斯修正的Ni
+Ni = amax(D[:, 0:6] , axis = 0).reshape(1,6)
 
 #计算好瓜时类条件概率
+#离散属性列
 x_goodWM_sep = zeros((1,6))
-
 for i in range(6):
     for j in range(n):
         if D[j,d-1] == 1 and test[0,i] == D[j,i]:
             x_goodWM_sep[0,i] += 1
+x_goodWM_sep = (x_goodWM_sep + 1)/(goodWM + Ni)
 
-x_goodWM_sep = x_goodWM_sep/goodWM
+#连续属性列
+mean_goodWM = D[0:8,6:8].mean(axis = 0).reshape(1,2)
+std_goodWM = D[0:8,6:8].std(axis = 0, ddof = 1).reshape(1,2)
+x_goodWM_con = 1/(sqrt(2*pi)*std_goodWM)*exp(-(test[0,6:8]-mean_goodWM)**2/(2*std_goodWM**2))
 
-x_goodWM_con = zeros((1,2))
-
-len(goodWM)
+x_goodWM = P_goodWM * prod(x_goodWM_sep)* prod(x_goodWM_con)
 
 #计算非好瓜时类条件概率
-x_badWM = zeros((1,6))
+x_badWM_sep = zeros((1,6))
 
 for i in range(6):
     for j in range(n):
         if D[j,d-1] == 0 and test[0,i] == D[j,i]:
-            x_badWM[0,i] += 1
+            x_badWM_sep[0,i] += 1
+x_badWM_sep = (x_badWM_sep + 1)/(badWM + Ni)
+#连续属性列
+mean_badWM = D[8:,6:8].mean(axis = 0).reshape(1,2)
+std_badWM = D[8:,6:8].std(axis = 0, ddof = 1).reshape(1,2)
+x_badWM_con = 1/(sqrt(2*pi)*std_badWM)*exp(-(test[0,6:8]-mean_badWM)**2/(2*std_badWM**2))
+x_badWM = P_badWM * prod(x_badWM_sep)* prod(x_badWM_con)
+
+print('好瓜的概率 = %s/n坏瓜的概率 = %s' %(x_goodWM, x_badWM))
